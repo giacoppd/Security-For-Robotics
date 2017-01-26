a8b2277829db776248f00f1648015872a1765544
Vedanth Narayanan <narayave@oregonstate.edu>
2017-01-26 10:05:12 -0800

    Made scripts executables, and package was missing necessary empty directories

---
+++ malpac/catkin_ws/src/malpac/pybits/spy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+import rospkg
+import rospy
+from std_msgs.msg import Int32
+
+def printer(data):
+	rospy.loginfo("saw %s", data.data)
+	print('I see' + str(data.data))
+	rospy.spin() #why doesn't it keep running
+
+def notmain():
+	rospy.init_node('spy', anonymous=True)
+	rospy.Subscriber('addout', Int32, printer)
+
+if __name__ == '__main__':
+	notmain()
