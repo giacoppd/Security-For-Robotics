715cccf7e0cff4aee3042ed590e845f8f30ea887
Vedanth Narayanan <narayave@oregonstate.edu>
2017-01-26 18:08:39 -0800

    We are now killing a node

--- malpac/catkin_ws/src/malpac/pybits/redirect.py
+++ malpac/catkin_ws/src/malpac/pybits/redirect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import time
+import os
 from std_msgs.msg import String
 
 def callback(data):
@@ -8,6 +9,15 @@ def callback(data):
 
 def repeat(data):
 	print "I am trying to read the following " + str(data.data)
+	
+
+	os.system("source ~/catkin_ws/devel/setup.bash")
+	rospy.loginfo("sourced bash file")
+	os.system("rosnode kill /talker")
+	rospy.loginfo("killed talker")
+
+
+
 	pub = rospy.Publisher('chatter', String, queue_size=10)
 	rate = rospy.Rate(100)
 	giveTime = "The time is " + str(time.clock())
@@ -17,5 +27,5 @@ def repeat(data):
 if __name__ == '__main__':
 	rospy.init_node('repeater', anonymous=True)
 	rospy.Subscriber("chatter", String, repeat)
-	rospy.Subscriber("chatter", String, callback)
+	# rospy.Subscriber("chatter", String, callback)
 	rospy.spin()
