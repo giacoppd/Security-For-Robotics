212beada02c6562bbfa160feabea7f567126b9fa
dominic <giacoppd@oregonstate.edu>
2017-02-21 23:24:07 -0800

    Cleaned up my code a bit, made some assumptions about sourcing

--- ros_packages/malpac6b/src/replacer.py
+++ ros_packages/malpac6b/src/replacer.py
@@ -2,6 +2,7 @@
 #this isn't important enough to license
 #Starts a "good" node in a different package, lets it run for 10 seconds, then kills it and runs a node with the same name from a different package
 #assumes your on ROS indigo
+#also assumes you've make'd, installed, and sourced all your files.
 import os
 import rospy
 import time
@@ -12,9 +13,6 @@ def swapper():
     os.system('export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:$PKG_CONFIG_PATH"')
     os.system('export ROS_HOSTNAME=localhost')
     os.system("export ROS_MASTER_URI=http://localhost:11311")
-    os.system("source ../malpac6/install/setup.bash")
-    os.system("cd ../")
-    os.system('catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so')
     os.system("rosrun malpac6 dummy.py &")
     time.sleep(10)
     os.system("kill $(ps aux | grep dummy.py | grep -v grep | awk '{print $2}')")
