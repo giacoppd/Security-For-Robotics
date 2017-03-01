44038188d28bb8f26f53b5ebb0a9bed983faf1a2
dominic <giacoppd@oregonstate.edu>
2017-02-28 19:50:21 -0800

    Deleted some build files we don't need

--- ros_packages/malpac3/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominic/SP/Security-For-Robotics/malpac3/src
-# Build directory: /home/dominic/SP/Security-For-Robotics/malpac3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("malpac3")
--- ros_packages/malpac3/build/malpac3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominic/SP/Security-For-Robotics/malpac3/src/malpac3
-# Build directory: /home/dominic/SP/Security-For-Robotics/malpac3/build/malpac3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
