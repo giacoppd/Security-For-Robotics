3b5de881e8ddc50a57be31163696e3026072ff98
Zach Rogers <zachrog@gmail.com>
2017-03-21 02:37:57 -0700

    Additional information for the netcat backdoor dropper, aka net kitty dropper ~

--- ros_packages/malpac_comms/README.md
+++ ros_packages/malpac_comms/README.md
@@ -2,5 +2,12 @@
 The purpose of this package is to compromise communications on a ROS based system. There will be several
 methods enclosed in this "package", which will be described as they are developed.
 
+### Methods
+  * nc_drop.py -- Netcat Backdoor Service Dropper. Will attempt to spin up netcat as a background process,
+  accepting incoming connections across any available communication interfaces.
+
+  Netcat will attempt to be ran if it is already on the system, or will try to run / compile a copy of Netcat
+  in memory, at run time.
+
 ### Notes
   * These will be put into an actual ROS / catkin package at another time
--- ros_packages/malpac_comms/nc_drop.py
+++ ros_packages/malpac_comms/nc_drop.py
@@ -1,10 +1,56 @@
 #!/usr/bin/python
-
+###################################################################################
+##                _____________
+##               /           ,.\
+##              /           /  \\
+##        o    /           {    }\
+##         `.  \ ....       \  / /
+##           `. \ \\\\       `' /
+##             \ \_____________/
+##              \      \ \
+##               \      \ \
+##                \     _\ \________________________
+##                (`\  /  \ \ ___         "-._       )
+##                 \ \/   /`-'  /,       /`-._"-._  /
+##                  `/    """"""'       /___ _"-._"-._
+##                  /___ __  _          `-._      '  /
+##                  \  RoboSec              "-._ /  /
+##                 __\________________________  "  /
+##               /    '//,  '//'               )__/
+##              /       '///'    ,//'/,       /
+##             (.---------------------------./
+##              `:.                        //
+##                `=======================''
+##                  Security for Robotics
+##
+##             Oregon State University - CS Capstone
+##
+##         Zach Rogers - Malicious ROS Package Collection
+##
+##                    Net Kitty Dropper :3c
+##                         nc_drop.py
+###################################################################################
 import os #for os.system calls
 #import rospy
 
+#Should we be loud and proud?
+VERBOSE = True
+
+def log(text):
+    """
+    Logging function - Will output text if VERBOSE is set to True, otherwise calls will be ignored.
+
+    Parameters:
+    --------------
+        * text - String of text to be logged
+
+    """
+
+    if VERBOSE:
+        print str(text)
+
 def main():
-    print "meow"
+    log("[~] net kitty dropper :3c")
 
 if __name__ == '__main__':
     main()
