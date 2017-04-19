c700f30838ef806301d78d8faaaff7bcdcfc3531
Zach Rogers <zachrog@gmail.com>
2017-04-18 19:15:07 -0700

    Start of another ROS PoC that will pull all of the publishers and subscribers from the victim machine. After doing this, all publisher data will be logged and all subscribers will be flooded with data (maybe fuzzing later on?)

--- ros_packages/malpac_ros/README.md
+++ ros_packages/malpac_ros/README.md
@@ -9,5 +9,8 @@ Here are the various proof of concepts each script / package is attempting to sh
 
   * ros2.py - PoC Broken Authentication 2 - Sets up a publisher on the victim machine, and a subscriber on the attacker machine. Showing that communication can occur between the two, without any authentication.
 
+#### Publisher / Subscriber Process Communication
+  * ros3.py - Will pull all the active publishers and subscribers on the victim machine. All of the subscribers will be flooded with random data (fuzzing?), and all of the publishers will have their data logged for later review (maybe integrate with ROS bag?)
+
 ### Notes
   * These will be put into an actual ROS / catkin package at another time
---
+++ ros_packages/malpac_ros/ros3.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+###################################################################################
+##                _____________
+##               /           ,.\
+##              /           /  \\
+##        o    /           {    }\
+##         `.  \ ....       \  / /
+##           `. \ \\\\       `' /
+##             \ \_____________/
+##              \      \ \
+##               \      \ \
+##                \     _\ \________________________
+##                (`\  /  \ \ ___         "-._       )
+##                 \ \/   /`-'  /,       /`-._"-._  /
+##                  `/    """"""'       /___ _"-._"-._
+##                  /___ __  _          `-._      '  /
+##                  \  RoboSec              "-._ /  /
+##                 __\________________________  "  /
+##               /    '//,  '//'               )__/
+##              /       '///'    ,//'/,       /
+##             (.---------------------------./
+##              `:.                        //
+##                `=======================''
+##                  Security for Robotics
+##
+##             Oregon State University - CS Capstone
+##
+##         Zach Rogers - Malicious ROS Package Collection
+##
+##             ROS Package 3 - Publisher Logging and Subscriber fuzzing
+##                         ros3.py
+###################################################################################
+import os
+import rospy
+from std_msgs.msg import String
+
+#ROS Environment Variables -- Will need to change these accordingly
+ROS_CORE_PORT = "1337"
+VICTIM_ROS_HOSTNAME = "192.168.0.10"
+VICTIM_ROS_MASTER_URI = "http://" + VICTIM_ROS_HOSTNAME + ":" + ROS_CORE_PORT
+
+ATTACKER_ROS_MASTER_URI = VICTIM_ROS_MASTER_URI
+ATTACKER_ROS_HOSTNAME = "192.168.0.11"
+
+#Should we be loud and proud?
+VERBOSE = True
+
+def log(text):
+    """
+    Logging function - Will output text if VERBOSE is set to True, otherwise calls will be ignored.
+
+    Parameters:
+    --------------
+        * text - String of text to be logged
+
+    """
+
+    if VERBOSE:
+        print str(text)
+
+
+def main():
+    log("[~] PoC showing Publisher Logging and Subscriber Fuzzing")
+
+
+if __name__ == '__main__':
+    main()
